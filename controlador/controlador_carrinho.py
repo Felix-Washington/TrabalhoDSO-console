18ef6452cca7b081da483030fd76a733bc4cb6e0
Francielle Lopes <franciellelps@gmail.com>
2020-10-29 17:40:16 +0000

    Criação do construtor e assinatura dos métodos do  ControladorCarrinho

--- controlador/controlador_carrinho.py
+++ controlador/controlador_carrinho.py
@@ -1,8 +1,20 @@
 from controlador.abstract_controlador import AbstractControlador
+from tela.tela_carrinho import TelaCarrinho
 
 class ControladorCarrinho(AbstractControlador):
 
   def __init__(self):
+
+    self.__tela_carrinho = TelaCarrinho(self)
+    self.__carrinhos = []
+
+  def abre_tela_inicial(self):
+    pass
+
+  def inicia (self):
+    pass
+
+  def lista(self):
     pass
 
   def adiciona(self):
@@ -13,3 +25,20 @@ class ControladorCarrinho(AbstractControlador):
 
   def atualiza(self):
     pass
+
+  def limpa_tela(self):
+    pass
+
+  def limpar_carrinho(self):
+    pass
+    
+  def valor_total(self):
+    pass
+
+  def finaliza_tela(self):
+    pass
+
+ 
+
+
+  

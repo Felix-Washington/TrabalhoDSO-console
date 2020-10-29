d70274c1fde5ff9bc325a89e5f64710700d3da09
Francielle Lopes <franciellelps@gmail.com>
2020-10-29 17:54:41 +0000

    TelaProduto

--- tela/tela_produto.py
+++ tela/tela_produto.py
@@ -1,13 +1,20 @@
 from tela.abstract_tela import AbstractTela
 
 class TelaProduto(AbstractTela):
-  def __init__(self):
-    pass
+    def __init__(self, controlador):
+      self.__controlador = controlador
 
-
-  def mostra_opcoes(self):
-    print("Você deseja:")
-    print("1 - Cadastrar Produto")
-    print("2 - Remover Produto")
-    print("3 - Remover Cadastro")
-    print("0 - Voltar")
+    def mostra_opcoes(self):
+      
+      print("------ PRODUTO ------")
+      print("1 - Adicionar Produto")
+      print("2 - Remover Produto")
+      print("3 - Atualizar Produto")
+      print("4 - Listar Produtos")
+      print("5 - Imprimir Relatório de Estoque")
+      print("0 - Voltar")
+    
+      opcao = self.le_numero_inteiro("Escolha a opção: ")
+      return opcao
+    def avisos(self):
+      pass

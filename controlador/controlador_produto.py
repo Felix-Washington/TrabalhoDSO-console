1c7f97b0507347c9e3d9778007bd246ad2532df7
Francielle Lopes <franciellelps@gmail.com>
2020-10-29 17:49:07 +0000

    Ajustes no construtor e métodos do ControladorProduto

--- controlador/controlador_produto.py
+++ controlador/controlador_produto.py
@@ -1,27 +1,40 @@
 from controlador.abstract_controlador import AbstractControlador
 
+from __tela_produto import TelaProduto
+
 class ControladorProduto(AbstractControlador):
+
   def __init__(self):
+    self.__tela_produto = TelaProduto(self)
     self.__produtos = []
+
+  def inicia(self):
     pass
 
   def adiciona(self):
     pass
 
-
   def remove(self):
     pass
-
 
   def atualiza(self):
     pass
 
+  def lista(self):
+    pass
 
-  @property
-  def produtos(self):
-    return self.__produtos
+  def abre_tela_inicial(self):
+    pass
+    
+  def finaliza_tela(self):
+    pass
 
+  def relatorio_estoque(self):
+    pass
 
-  @produtos.setter
-  def produtos(self, produtos):
-    self.__produtos = produtos
+  def atualiza_estoque(self):
+    pass
+    
+  def imprime_relatorio(self):
+    pass
+  

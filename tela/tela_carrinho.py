e00ca15a6bb54b73514e41d1434d712b873579fe
Francielle Lopes <franciellelps@gmail.com>
2020-10-29 17:53:12 +0000

    TelaCarrinho

--- tela/tela_carrinho.py
+++ tela/tela_carrinho.py
@@ -1,17 +1,22 @@
 from tela.abstract_tela import AbstractTela
 
-class TelaCarrinho(AbstractTela):
-  def __init__(self):
-    pass
+class TelaCarrinho(AbstractTela)
 
-
+  def __init__(self, controlador):
+    self.__controlador = controlador
 
   def mostra_opcoes(self):
-    print("Você deseja:")
-    print("1 - Adicionar Produto")
-    print("2 - Remover Produto")
-    print("3 - Limpar Carrinho")
+    print("------ REALIZAR COMPRA ------")
+    print("1 - Listar produtos disponíveis")
+    print("2 - Adicionar produto")
+    print("3 - Remover produto")
+    print("4 - Atualizar quantidade")
+    print("5 - Limpar carrinho")
+    print("6 - Finalizar compra")
     print("0 - Voltar")
-
-    opcao = self.le_numero_inteiro("Escolha a opcao: ", [1, 2, 3, 0])
+    
+    opcao = self.le_numero_inteiro("Escolha a opção: ")
     return opcao
+
+  def avisos(self):
+    pass
